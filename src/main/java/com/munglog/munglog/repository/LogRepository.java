03d707e80be3c0e8cea5605f16f4c8a414d2fb5c
안효경 <dksgyrud1349@naver.com>
2026-06-08 13:55:09 +0900

    fix package structure

--- src/main/java/com/munglog/munglog/repository/LogRepository.java
+++ src/main/java/com/munglog/munglog/repository/LogRepository.java
@@ -1,8 +1,8 @@
-package com.munglog.repository;
+package com.munglog.munglog.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.munglog.document.Log;
+import com.munglog.munglog.document.Log;
 
 public interface LogRepository extends MongoRepository<Log, String> {
 }
